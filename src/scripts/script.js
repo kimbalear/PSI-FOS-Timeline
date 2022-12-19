61edfc9b01df8e2b869204b5481340ececac8547
Joaquin Urbano Serra <kimbalear@gmail.com>
2022-12-19 18:42:31 +0100

    scripts

---
+++ src/scripts/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    $('#lgnd1').click(function () {
+        $(".card_lgnd1").toggle(this.checked);
+    });
+
+    $('#lgnd2').click(function () {
+        $(".card_lgnd2").toggle(this.checked);
+    });
+
+    $('#lgnd3').click(function () {
+        $(".card_lgnd3").toggle(this.checked);
+    });
+
+    $('#lgnd4').click(function () {
+        $(".card_lgnd4").toggle(this.checked);
+    });
+
+    $('#lgnd5').click(function () {
+        $(".card_lgnd5").toggle(this.checked);
+    });
+
+    $('#lgnd6').click(function () {
+        $(".card_lgnd6").toggle(this.checked);
+    });
+
+});
