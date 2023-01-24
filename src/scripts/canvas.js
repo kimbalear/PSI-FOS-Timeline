56266a989dfb3309db0978cf6d842aba7ef0337f
Joaquin Urbano Serra <kimbalear@gmail.com>
2023-01-24 15:23:37 +0100

    ok

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -4,21 +4,18 @@ $(document).ready(function () {
 
     // Add click event for zoom in button
     $(document).on('click', '#zoomIn', function () {
-        console.log("zoomIn")
-        zoom += 0.1;
-        $("#myCanvas").css("transform", "scale(" + zoom + ")");
+        zoom += 0.1
+        $("#myCanvas").css("transform", "scale(" + zoom + ")")
     });
 
     // Add click event for zoom out button
     $(document).on('click', '#zoomOut', function () {
-        console.log("zoomOut")
-        zoom -= 0.1;
-        $("#myCanvas").css("transform", "scale(" + zoom + ")");
+        zoom -= 0.1
+        $("#myCanvas").css("transform", "scale(" + zoom + ")")
     })
 
     $(document).on('click', '#zoom-reset', function () {
-        console.log("reset")
-        $("#myCanvas").css("transform", "scale(1)");
-        zoom = 1;
+        $("#myCanvas").css("transform", "scale(1)")
+        zoom = 1
     })
 })
